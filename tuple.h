06188cac82231ac57038199ea8f6144d8d9eac04
WhoAmI-FK <Faismen2004@mail.ru>
2022-08-14 12:26:35 +0200

    initial commit

---
+++ tuple.h
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace __whoAmIFk_tuple {
+	template<std::size_t index,typename T>
+	class _tuple_impl
+	{
+	public:
+		typedef T const& const_ref;
+		typedef T&& move_type;
+		typedef T& ref;
+		typedef T val_type;
+		_tuple_impl(const_ref v)
+		{
+			_val = v;
+		}
+
+		_tuple_impl(move_type v)
+		{
+			_val = std::move(v);
+		}
+
+		ref get()
+		{
+			return _val;
+		}
+	private:
+		val_type _val;
+	};
+
+	template<std::size_t index, typename L, typename... types>
+	class _tuple_recurr_base :
+		public _tuple_impl < index, typename std::remove_reference<L>::type>,
+		public _tuple_recurr_base<index + 1, types...>
+	{
+
+	};
+};
---
+++ tuple_cpp.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "tuple.h"
+#include <string>
+#include <iostream>
+
+int main()
+{
+	tuple t3{ 1, 2.0, std::string("hello"),std::string("world") };
+	std::cout << get<0>(t) << ", " << get<1>(t) << ", " << get<2>(t) << ", " << get<3>(t2) << "\n";
+
+	tuple t2{ 1, 2.0, std::string(“hello”), std::string(“world”) };
+	std::cout << std::boolalpha << "tuples are equal : " << (t2 == t3) << "\n";
+
+	return 0;
+}
